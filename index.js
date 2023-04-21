35408c3508ddafb3253f9804345d80461c719263
Atul Agnihotri <116969956+atul240@users.noreply.github.com>
2023-04-21 13:55:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// var player1 = prompt("Enter Player1 name: ")
+// var player2 = prompt("Enter Player2 name: ")
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice"+randomNumber1+".png";
+
+var randomImageSource = "images/"+ randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0]
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice"+randomNumber2+".png";
+
+var randomImageSource = "images/"+ randomDiceImage;
+
+var image2 = document.querySelectorAll("img")[1]
+
+image2.setAttribute("src", randomImageSource);
+
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = ("player1" +" "+"is Winner 🥇")
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = ("player2" +" "+"is Winner 🥇")
+}
+else{
+    document.querySelector("h1").innerHTML = "Tie"
+}
